1d72828c67a1683897f0d9b707c507d8af755a38
Sebastian Höffner <info@sebastian-hoeffner.de>
2017-08-08 18:24:27 +0200

    Adding input pipeline for mnist data

--- src/data.py
+++ src/data.py
@@ -17,9 +17,16 @@ def _get_pipeline(dataset):
                        labels=None,
                        reader=lambda q: _read(q, tf.TFRecordReader()),
                        convert=_convert_img_depth)
+    mnist = Pipeline(files=lambda *a: _files_list_dir(*a, filter='images'),
+                     labels=lambda *a: _files_list_dir(*a, filter='labels'),
+                     reader=lambda q0, q1:
+                         _read(q0, tf.FixedLengthRecordReader(28 * 28, 16),
+                               q1, tf.FixedLengthRecordReader(1, 8)),
+                     convert=_convert_mnist)
     pipelines = {
         'make3d1': default,
         'make3d2': default,
+        'mnist': mnist,
         'nyu': default,
     }
     return pipelines.get(dataset, default)
@@ -53,6 +60,11 @@ def _files_tfrecords(base_dir, train_or_test='train'):
     return [os.path.join(base_dir, f'{train_or_test}.tfrecords')]
 
 
+def _files_list_dir(base_dir, train_or_test='train', filter=''):
+    dir = os.path.join(base_dir, train_or_test)
+    return [os.path.join(dir, p) for p in os.listdir(dir) if filter in p]
+
+
 def _read(queue, queue_reader, label_queue=None, label_reader=None):
     key, record = queue_reader.read(queue)
     label = None
@@ -81,9 +93,19 @@ def _convert_img_depth(key, record, *args):
     return image, depth
 
 
+def _convert_mnist(key, record, label):
+    image = tf.parse_single_example(record, features={
+                'data': tf.FixedLenFeature([], tf.string),
+            })
+    image = tf.decode_raw(image['data'], tf.uint8)
+    image = tf.reshape(image, [28, 28, 1])
 
-    return tf.train.shuffle_batch([image, depth],
-                                  batch_size=batch_size,
-                                  capacity=4*batch_size,
-                                  min_after_dequeue=2*batch_size,
-                                  num_threads=2)
+    # Make static size
+    image = tf.image.resize_images(image, [28, 28])
+    image = tf.cast(image, tf.float32) / 255. - .5
+    label = tf.parse_single_example(record, features={
+                'label': tf.FixedLenFeature([], tf.string),
+            })
+    label = tf.decode_raw(label['label'], tf.uint8)
+    label = tf.reshape(label, [1])
+    return image, label
