bd61cfd3609937c62a1a23c894802d510d53b360
Sebastian Höffner <info@sebastian-hoeffner.de>
2017-08-18 16:54:29 +0200

    simpler conda installation process

--- tools/grid/setup-conda-env.sh
+++ tools/grid/setup-conda-env.sh
@@ -6,13 +6,13 @@ CONDAENVCPU=${CONDAENV-ann3depth}-CPU
 
 conda create -n $CONDAENVGPU --yes python=3.6
 source activate $CONDAENVGPU
+conda info --envs | grep '*'
 
-pip install --upgrade --ignore-installed -r requirements.txt
-pip install --upgrade --ignore-installed https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.2.1-cp36-cp36m-linux_x86_64.whl
+pip install --upgrade -r requirements.txt -r requirements-gpu.txt
 
 
 conda create -n $CONDAENVCPU --yes python=3.6
 source activate $CONDAENVCPU
+conda info --envs | grep '*'
 
-pip install --upgrade --ignore-installed -r requirements.txt
-pip install --upgrade --ignore-installed https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.2.1-cp36-cp36m-linux_x86_64.whl
+pip install --upgrade -r requirements.txt -r requirements-cpu.txt
