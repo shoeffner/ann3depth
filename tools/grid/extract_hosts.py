3bfbd24a3da436170621dd7b01297ca8f203244f
Sebastian Höffner <info@sebastian-hoeffner.de>
2017-08-19 15:44:34 +0200

    fixing extract_hosts.py to also take limited queues into account

--- tools/grid/extract_hosts.py
+++ tools/grid/extract_hosts.py
@@ -1,10 +1,19 @@
 import sys
 
+def check_queue():
+    queues = []
+    if len(sys.argv) > 1 and sys.argv[1] == '-q':
+        queues = sys.argv[2].split(',')
+    return queues
+
 if __name__ == '__main__':
+    allowed_queues = check_queue()
     hosts = []
     last_line = 'x x 0/0/0 x x x'
     for l in sys.stdin:
         if l.startswith('\tqf:hostname='):
+            if allowed_queues and last_line.split('@')[0] not in allowed_queues:
+                continue
             slots = last_line.split()[2].split('/')
             if int(slots[0]) + int(slots[1]) < int(slots[2]):
                 hosts.append(l[len('\tqf:hostname='):-1])
